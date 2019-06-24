996d11730805dc9d9480f25741796e7966ff8c03
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2019-06-24 17:55:03 +0200

    fix: Default response status to 200

--- lib/door_frame/response.ex
+++ lib/door_frame/response.ex
@@ -1,7 +1,7 @@
 defmodule DoorFrame.Response do
   defstruct client: nil,
             resource_owner: nil,
-            status: nil,
+            status: 200,
             access_token: nil,
             token_type: nil,
             expires_in: nil
