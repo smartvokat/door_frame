24ee2939c9178b8c94a1d0ece9c1175f2c1c38f7
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2019-06-28 11:39:07 +0200

    refact: Move common grant type code to base module

---
+++ lib/door_frame/grant_type/base.ex
@@ -0,0 +1,87 @@
+defmodule DoorFrame.GrantType.Base do
+  @moduledoc false
+
+  alias DoorFrame.Error
+  alias DoorFrame.Request
+  alias DoorFrame.Response
+
+  @type token_type :: :access_token | :refresh_token
+
+  @spec validate_scope(DoorFrame.Request.t()) ::
+          {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Request.t()}
+  def validate_scope(request) do
+    case request.handler.validate_scope(request.scope) do
+      {:ok} -> {:ok, request}
+      {:ok, %Request{} = request} -> {:ok, request}
+      {:ok, scope} -> {:ok, Map.put(request, :scope, scope)}
+      {:error, description} -> {:error, Error.invalid_scope(description)}
+      {:error} -> {:error, Error.invalid_scope()}
+    end
+  end
+
+  @spec get_client(DoorFrame.Request.t(), DoorFrame.Response.t()) ::
+          {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Response.t()}
+  def get_client(request, response) do
+    case request.handler.get_client(request, response) do
+      {:ok, %Response{} = response} -> {:ok, response}
+      {:ok, client} -> {:ok, Map.put(response, :client, client)}
+      {:error, description} -> {:error, Error.invalid_client(description)}
+      {:error} -> {:error, Error.invalid_client()}
+    end
+  end
+
+  @spec get_resource_owner_from_client(DoorFrame.Request.t(), DoorFrame.Response.t()) ::
+          {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Response.t()}
+  def get_resource_owner_from_client(request, response) do
+    if supports?(request.handler, :get_resource_owner_from_client, 2) do
+      case request.handler.get_resource_owner_from_client(request, response) do
+        {:ok, %Response{} = response} -> {:ok, response}
+        {:ok, resource_owner} -> {:ok, Map.put(response, :resource_owner, resource_owner)}
+        {:error, description} -> {:error, Error.invalid_client(description)}
+        {:error} -> {:error, Error.invalid_client()}
+      end
+    else
+      {:ok, response}
+    end
+  end
+
+  @spec generate_token(token_type, DoorFrame.Request.t(), DoorFrame.Response.t()) ::
+          {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Response.t()}
+  def generate_token(type, request, response) do
+    case request.handler.generate_token(type, request, response) do
+      {:ok, %Response{} = response} ->
+        {:ok, response}
+
+      {:ok, token} ->
+        {:ok, Map.put(response, type, token)}
+
+      {:error, description} ->
+        {:error, Error.server_error(description)}
+
+      {:error} ->
+        {:error, Error.server_error()}
+    end
+  end
+
+  @spec persist_tokens(DoorFrame.Request.t(), DoorFrame.Response.t()) ::
+          {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Response.t()}
+  def persist_tokens(request, response) do
+    tokens = %{access_token: response.access_token, refresh_token: response.refresh_token}
+
+    case request.handler.persist_tokens(tokens, response) do
+      {:ok} ->
+        {:ok, response}
+
+      {:ok, %Response{} = response} ->
+        {:ok, response}
+
+      {:error, description} ->
+        {:error, Error.server_error(description)}
+
+      {:error} ->
+        {:error, Error.server_error()}
+    end
+  end
+
+  def supports?(handler, func, arity), do: :erlang.function_exported(handler, func, arity)
+end
--- lib/door_frame/grant_type/client_credentials.ex
+++ lib/door_frame/grant_type/client_credentials.ex
@@ -10,17 +10,18 @@ defmodule DoorFrame.GrantType.ClientCredentials do
   alias DoorFrame.Error
   alias DoorFrame.Request
   alias DoorFrame.Response
+  alias DoorFrame.GrantType.Base
 
   @spec handle(DoorFrame.Request.t(), DoorFrame.Response.t()) ::
           {:error, DoorFrame.Error.t()} | {:ok, DoorFrame.Response.t()}
   def handle(%Request{} = request, %Response{} = response) do
     with {:ok} <- validate_request(request),
-         {:ok, request} <- validate_scope(request),
-         {:ok, response} <- get_client(request, response),
-         {:ok, response} <- get_resource_owner_from_client(request, response),
-         {:ok, response} <- generate_token(:access_token, request, response),
-         {:ok, response} <- generate_token(:refresh_token, request, response),
-         {:ok, response} <- persist_tokens(request, response) do
+         {:ok, request} <- Base.validate_scope(request),
+         {:ok, response} <- Base.get_client(request, response),
+         {:ok, response} <- Base.get_resource_owner_from_client(request, response),
+         {:ok, response} <- Base.generate_token(:access_token, request, response),
+         {:ok, response} <- Base.generate_token(:refresh_token, request, response),
+         {:ok, response} <- Base.persist_tokens(request, response) do
       {:ok, response}
     else
       {:error, %Error{}} = e -> e
@@ -39,72 +40,4 @@ defmodule DoorFrame.GrantType.ClientCredentials do
         {:ok}
     end
   end
-
-  defp validate_scope(request) do
-    case request.handler.validate_scope(request.scope) do
-      {:ok} -> {:ok, request}
-      {:ok, %Request{} = request} -> request
-      {:ok, scope} -> {:ok, Map.put(request, :scope, scope)}
-      {:error, description} -> {:error, Error.invalid_scope(description)}
-      {:error} -> {:error, Error.invalid_scope()}
-    end
-  end
-
-  defp get_client(request, response) do
-    case request.handler.get_client(request, response) do
-      {:ok, %Response{} = response} -> response
-      {:ok, client} -> {:ok, Map.put(response, :client, client)}
-      {:error, description} -> {:error, Error.invalid_client(description)}
-      {:error} -> {:error, Error.invalid_client()}
-    end
-  end
-
-  defp get_resource_owner_from_client(request, response) do
-    if supports?(request.handler, :get_resource_owner_from_client, 2) do
-      case request.handler.get_resource_owner_from_client(request, response) do
-        {:ok, %Response{} = response} -> response
-        {:ok, resource_owner} -> {:ok, Map.put(response, :resource_owner, resource_owner)}
-        {:error, description} -> {:error, Error.invalid_client(description)}
-        {:error} -> {:error, Error.invalid_client()}
-      end
-    else
-      {:ok, response}
-    end
-  end
-
-  defp generate_token(type, request, response) do
-    case request.handler.generate_token(type, request, response) do
-      {:ok, %Response{} = response} ->
-        response
-
-      {:ok, token} ->
-        {:ok, Map.put(response, type, token)}
-
-      {:error, description} ->
-        {:error, Error.server_error(description)}
-
-      {:error} ->
-        {:error, Error.server_error()}
-    end
-  end
-
-  defp persist_tokens(request, response) do
-    tokens = %{access_token: response.access_token, refresh_token: response.refresh_token}
-
-    case request.handler.persist_tokens(tokens, response) do
-      {:ok} ->
-        {:ok, response}
-
-      {:ok, %Response{} = response} ->
-        response
-
-      {:error, description} ->
-        {:error, Error.server_error(description)}
-
-      {:error} ->
-        {:error, Error.server_error()}
-    end
-  end
-
-  defp supports?(handler, func, arity), do: :erlang.function_exported(handler, func, arity)
 end
