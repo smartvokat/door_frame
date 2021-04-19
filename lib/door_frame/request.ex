daa0dc7e2d9e6ce31327daceb6bda329e732b1bc
Christian Schuhmann <97274+madebyherzblut@users.noreply.github.com>
2021-04-19 09:10:28 +0200

    refact: Add initial typespec for `Request` struct

--- lib/door_frame/request.ex
+++ lib/door_frame/request.ex
@@ -1,13 +1,27 @@
 defmodule DoorFrame.Request do
+  @type t() :: %__MODULE__{
+          access_token_lifetime: integer(),
+          available_grant_types: map(),
+          client_id: binary(),
+          client_secret: binary(),
+          context: any(),
+          grant_type: binary(),
+          handler: any(),
+          password: binary(),
+          refresh_token_lifetime: integer(),
+          scope: any(),
+          username: binary()
+        }
+
   defstruct access_token_lifetime: 60 * 60 * 4,
             available_grant_types: %{},
             client_id: nil,
             client_secret: nil,
+            context: nil,
             grant_type: nil,
             handler: nil,
             password: nil,
             refresh_token_lifetime: 60 * 60 * 24 * 14,
             scope: nil,
-            username: nil,
-            context: nil
+            username: nil
 end
